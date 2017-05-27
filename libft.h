2f7e19bc474878719f4340db907f9d7016f8b653
Yoann Ginet <yoann@MacBook-Pro.local>
2017-05-27 17:02:11 +0200

    maj 27052017

--- libft.h
+++ libft.h
@@ -18,28 +18,28 @@
 
 /* Fonctions standard */
 
-void	*ft_memset(void *str, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void  *ft_memcpy(void *str1, const void *str2, size_t n);
-void  *ft_memccpy(void *dest, const void *src, int c, size_t n);
-void  *ft_memmove(void *str1, const void *str2, size_t n);
-void *ft_memchr(const void *str, int c, size_t n);
-int   ft_memcmp(const void *str1, const void *str2, size_t n);
+void    *ft_memset(void *str, int c, size_t n);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *str1, const void *str2, size_t n);
+void    *ft_memccpy(void *dest, const void *src, int c, size_t n);
+void    *ft_memmove(void *str1, const void *str2, size_t n);
+void    *ft_memchr(const void *str, int c, size_t n);
+int     ft_memcmp(const void *str1, const void *str2, size_t n);
 size_t  ft_strlen(const char *s);
-char  *strdup(const char *string);
-char  *ft_strcpy(char *dest, const char *src);
-char  *ft_strncpy(char *dest, const char *src, size_t n);
-char  *ft_strcat(char *dest, const char *src);
-char  *ft_strncat(char *dest, const char *src, size_t n);
+char    *strdup(const char *string);
+char    *ft_strcpy(char *dest, const char *src);
+char    *ft_strncpy(char *dest, const char *src, size_t n);
+char    *ft_strcat(char *dest, const char *src);
+char    *ft_strncat(char *dest, const char *src, size_t n);
 size_t  ft_strlcat(char *dst, const char *src, size_t size);
 /*
 strchr
 strrchr */
-char   *ft_strstr(const char *haystack, const char *needle);
-char   *ft_strnstr(const char *haystack, const char *needle, size_t len);
-int   ft_strcmp(const char *str1, const char *str2);
-int   ft_strncmp(const char *str1, const char *str2, size_t n);
-int   ft_atoi(const char *str);
+char    *ft_strstr(const char *haystack, const char *needle);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+int     ft_strcmp(const char *str1, const char *str2);
+int     ft_strncmp(const char *str1, const char *str2, size_t n);
+int     ft_atoi(const char *str);
 /* isalpha
 isdigit
 isalnum
