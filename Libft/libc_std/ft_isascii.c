a7498089a7fb24a30094f5ce317a16bd13a0a4f7
Yoann Ginet <yoginet@MacBook-Pro.local>
2017-12-03 17:14:06 +0100

    error

--- Libft/libc_std/ft_isascii.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int   ft_isascii(int c)
-{
-  if (c >= 0 && c <= 177)
-    return (c);
-  else
-    return (0);
-}
--- Libft/libc_std/ft_isprint.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int ft_isprint(int c)
-{
-  if(c >= 40 && c <= 176)
-    return (c);
-  else
-    return (0);
-}
