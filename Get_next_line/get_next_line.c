ef664c6d31765f3119cca1508ac087417b2d3c77
Yoann Ginet <yoginet@z4r3p3.le-101.fr>
2017-12-05 15:30:45 +0100

    pas encore sa

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                  .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2017/12/02 10:52:49 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/04 19:16:04 by yoginet          ###   ########.fr       */
+/*   Created: 2017/12/05 10:29:54 by yoginet      #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/05 15:30:32 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,33 +15,35 @@
 
 static int		ft_get_nl(char *s, char **line)
 {
-    int     i;
-    char    cpy;
+	int		i;
+	char	cpy;
 
-    i = 0;
-    while (s[i] || s[i] != '\n')
-        i++;
-    if (i > 0)
-    {
-        *line = ft_strsub(s, 0, i);
-        return (1);
-    }
-    else
-        *line = ft_strnew("");
+	i = 0;
+	while (s[i] != '\0' || s[i] != '\n')
+		i++;
+	ft_putnbr(i);
+	if (i > 0)
+	{
+		*line = ft_strsub(s, 0, i);
+		free(s);
+		return (1);
+	}
+	else
+		*line = ft_strdup("");
 	return (0);
 }
 
-static char     *ft_realloc(char *str, int size)
+static char		*ft_realloc(char *str, int size)
 {
-    char    *cpy;
+	char	*cpy;
 
-    if (size == 0 || !str)
-        return (NULL);
-    if (!(cpy = (char *)malloc(sizeof(char) * size)))
-        return (NULL);
-    ft_strcpy(cpy, str);
-    free(str);
-    return (cpy);
+	if (size == 0 || !str)
+		return (NULL);
+	if (!(cpy = (char *)malloc(sizeof(char) * size)))
+		return (NULL);
+	ft_strcpy(cpy, str);
+	free(str);
+	return (cpy);
 }
 
 static char		*ft_read_doc(const int fd, char *s, char *buf, int index)
@@ -50,10 +52,14 @@ static char		*ft_read_doc(const int fd, char *s, char *buf, int index)
 
 	while ((ret = read(fd, buf, BUFF_SIZE)) != 0)
 	{
-        index += BUFF_SIZE;
 		buf[ret] = '\0';
-        ft_strjoin(s, buf);
-        s = ft_realloc(s, index + BUFF_SIZE);
+		if (index == 0)
+		{
+			s = ft_strdup(buf);
+		}
+		s = ft_strjoin(s, buf);
+		index += BUFF_SIZE;
+		ft_realloc(s, (index + BUFF_SIZE));
 	}
 	return (s);
 }
@@ -62,9 +68,9 @@ static int		ft_error(const int fd, char **line, char *s)
 {
 	if (fd < 0)
 		return (1);
-    if (!line)
-        return (1);
-    s = ft_strnew(BUFF_SIZE);
+	if (!line)
+		return (1);
+	s = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
 	if (s == NULL)
 		return (1);
 	return (0);
@@ -73,14 +79,14 @@ static int		ft_error(const int fd, char **line, char *s)
 int				get_next_line(const int fd, char **line)
 {
 	static char		*s;
-    static int      index;
+	static int		index;
 	char			buff[BUFF_SIZE + 1];
 
-    index = 0;
+	index = 0;
 	if (ft_error(fd, line, s) == 1)
 		return (-1);
 	s = ft_read_doc(fd, s, buff, index);
-    if (ft_get_nl(s, line) == 1)
-        return (1);
+	if (ft_get_nl(s, line) == 1)
+		return (1);
 	return (0);
 }
--- Get_next_line/main.c
+++ Get_next_line/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.c                                           .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2017/12/02 10:53:10 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/04 14:40:11 by yoginet          ###   ########.fr       */
+/*   Created: 2017/12/05 10:30:22 by yoginet      #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/05 15:26:37 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,35 +16,31 @@
 
 int		main(int argc, char **argv)
 {
-    char	*line;
-    int		fd;
-    int		i;
+	char	**line;
+	int		fd;
 
-    i = 0;
-    if (argc == 2)
-    {
-        fd = open(argv[1], O_RDONLY);
-        line = NULL;
-        if (fd)
-        {
-            printf("Ouverture ok\n");
-            while ((i = get_next_line(fd, &line)) == 1)
-            {
-                printf("Valeur de i = |%d|\n", i);
-                printf("Line = |%s|\n", line);
-                free(line);
-                line = NULL;
-            }
-            close(fd);
-            printf("Fermeture ok\n");
-        }
-        if (fd == -1)
-        {
-            printf("Error read documents\n");
-            return (1);
-        }
-    }
-    else
-        printf("usage : ./a.out <target file>\n");
-    return (0);
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		line = NULL;
+		if (fd)
+		{
+			printf("Ouverture ok\n\n\n");
+			while (get_next_line(fd, line) == 1)
+			{
+				printf("Affichage de Line :\n");
+				printf("Line = |%s|\n", *line);
+			}
+			close(fd);
+			printf("\n\n\nFermeture ok\n");
+		}
+		if (fd == -1)
+		{
+			printf("Error read documents\n");
+			return (1);
+		}
+	}
+	else
+		printf("usage : ./a.out <target file>\n");
+	return (0);
 }
