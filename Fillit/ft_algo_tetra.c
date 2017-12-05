b5a183de56834eed0afbf1fc5b275aebdc469308
Yoann Ginet <yoginet@z4r3p3.le-101.fr>
2017-12-05 15:28:46 +0100

    100/100

--- Fillit/ft_algo_tetra.c
+++ Fillit/ft_algo_tetra.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_algo_tetra.c                                    :+:      :+:    :+:   */
+/*   ft_algo_tetra.c                                  .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: kcabus <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2017/11/28 15:50:47 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/04 16:14:50 by yoginet          ###   ########.fr       */
+/*   Created: 2017/12/04 15:59:54 by kcabus       #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/05 09:42:04 by kcabus      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static void   ft_del_lst(t_list *lst)
+static void		ft_del_lst(t_list *lst)
 {
-    while (lst)
-    {
-        free(lst->block);
-        free(lst->content);
-        free(lst);
-        lst = lst->next;
-    }
+	while (lst)
+	{
+		free(lst->block);
+		free(lst->content);
+		free(lst);
+		lst = lst->next;
+	}
 }
 
 static char		**ft_remplissage(t_list *lst, char **tab, int len, int p)
@@ -51,14 +51,14 @@ static char		**ft_remplissage(t_list *lst, char **tab, int len, int p)
 
 void			ft_algo_tetra(t_list *lst, int i)
 {
-    t_list  *remove;
+	t_list	*remove;
 	char	**tab;
 	int		pos;
 
-    remove = lst;
+	remove = lst;
 	pos = 0;
 	tab = NULL;
 	tab = ft_remplissage(lst, tab, i, pos);
 	free(tab);
-    ft_del_lst(remove);
+	ft_del_lst(remove);
 }
