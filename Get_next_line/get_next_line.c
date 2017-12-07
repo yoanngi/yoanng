710643e5561d3589a5972d29795011840c193154
Yoann Ginet <yoginet@z4r3p3.le-101.fr>
2017-12-07 13:22:37 +0100

    Sa marche

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,44 +6,40 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/05 10:29:54 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/07 12:06:57 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/07 13:21:17 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-static int		ft_get_nl(char **s, char **line)
+static int		ft_get_nl(char **s, char **line, int i)
 {
-	int		i;
 	int		len;
 	char	*cpy;
 
-	i = 0;
 	cpy = ft_strdup(*s);
 	len = ft_strlen(cpy);
-	//printf("Valeur de cpy : \n\n|%s|\n\n\n", cpy);
 	while (i < len + 1 && cpy[i] != '\n')
 		i++;
-	if (i > 0)
+	if (len == 0)
+		return (0);
+	else if (i > 0)
 	{
 		*line = ft_strsub(cpy, 0, i);
 		ft_strdel(s);
 		*s = ft_strsub(cpy, i + 1, len - i);
 		ft_strdel(&cpy);
-		return (1);
+		ft_strdel(&cpy);
 	}
-	else if (i == 0 && len != 0)
+	else
 	{
 		*line = ft_strdup("");
 		ft_strdel(s);
 		*s = ft_strsub(cpy, 1, len - 1);
 		ft_strdel(&cpy);
-		return (1);
 	}
-	else
-		return (0);
+	return (1);
 }
 
 static char		*ft_realloc(char *str, int size)
@@ -85,11 +81,13 @@ int				get_next_line(const int fd, char **line)
 	static char		*s;
 	char			buff[BUFF_SIZE + 1];
 	int				retour;
+	int				i;
 
+	i = 0;
 	if (fd < 0 || !line)
 		return (-1);
 	if (!s)
 		s = ft_read_doc(fd, s, buff);
-	retour = ft_get_nl(&s, line);
+	retour = ft_get_nl(&s, line, i);
 	return (retour);
 }
