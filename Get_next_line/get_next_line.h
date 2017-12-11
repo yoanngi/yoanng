668c0dacb9d04f14d32907ee0a06175f4dae401c
Yoann Ginet <yoginet@MacBook-Pro.local>
2017-12-11 18:32:20 +0100

    Modif .h

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/05 10:30:05 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/05 14:17:40 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/11 18:31:59 by yoginet          ###   ########.fr       */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 10
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <sys/stat.h>
-# include <sys/types.h>
-# include <fcntl.h>
 # include "libft/libft.h"
-
-# define BUFF_SIZE 10
 
 int		get_next_line(const int fd, char **line);
 
