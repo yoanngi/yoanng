974c23b3aa919292e49e0809873814c46b732109
Yoann Ginet <yoginet@MacBook-Pro.local>
2017-12-11 19:00:05 +0100

    9 errors, 1136 leaks

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/05 10:29:54 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/11 16:21:00 by yoginet          ###   ########.fr       */
+/*   Updated: 2017/12/11 18:58:46 by yoginet          ###   ########.fr       */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -54,7 +54,28 @@ static char		*ft_realloc(char *str, int size)
 	return (str);
 }
 
-static char		*ft_read_doc(const int fd, char *s, char *buf)
+static	char	*ft_strjoin_free(char *buf, char *s)
+{
+	size_t  len_buf;
+	size_t  len_s;
+	char	*str;
+
+	len_buf = 0;
+	len_s = 0;
+	if (buf)
+		len_buf = ft_strlen(buf);
+	if (s)
+		len_s = ft_strlen(s);
+	str = (char *)malloc(sizeof(*str) * (len_buf + len_s + 1));
+	ft_memcpy(str, buf, len_buf);
+	ft_memcpy(str + len_buf, s, len_s);
+	str[len_buf + len_s] = '\0';
+	free(buf);
+	ft_bzero(s, BUFF_SIZE + 1);
+	return (str);
+}
+
+static char		*ft_read_doc(const int fd, char **s, char *buf)
 {
 	int	ret;
 	int	index;
@@ -64,30 +85,29 @@ static char		*ft_read_doc(const int fd, char *s, char *buf)
 	{
 		buf[ret] = '\0';
 		if (index == 0)
-			s = ft_strdup(buf);
+			*s = ft_strdup(buf);
 		else
-			s = ft_strjoin(s, buf);
+			*s = ft_strjoin_free(*s, buf);
 		index += BUFF_SIZE;
-		ft_realloc(s, (index + BUFF_SIZE));
+        ft_realloc(*s, (index + BUFF_SIZE));
 	}
-    free(buf);
-	return (s);
+	return (*s);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	static char		*s;
-	char			buff[BUFF_SIZE + 1];
+	char			*buff;
 	int				retour;
 	int				i;
 
 	i = 0;
-	if (fd < 0 || !line || BUFF_SIZE <= 0)
+    buff = ft_strnew(BUFF_SIZE + 1);
+	if (fd < 0 || !line || BUFF_SIZE <= 0 || (read(fd, buff, 0)) < 0)
 		return (-1);
 	if (!s)
-		s = ft_read_doc(fd, s, buff);
+		s = ft_read_doc(fd, &s, buff);
+    ft_strdel(&buff);
 	retour = ft_get_nl(&s, line, i);
-	if (retour == 0 && s)
-		ft_strdel(&s);
 	return (retour);
 }
