8c429d1b28a1df015066fcacda4d9a254f24b2c2
Yoann Ginet <yoginet@z4r3p1.le-101.fr>
2017-12-15 15:45:50 +0100

    debut

--- Ft_printf/ft_printf.c
+++ Ft_printf/ft_printf.c
@@ -6,14 +6,25 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/12 13:37:05 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/12 16:34:37 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/15 15:45:38 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static s_struct		ft_insert_params(char *str)
+{
+	s_struct	*new;
+
+	return (new);
+}
+
 int		ft_printf(const char *format, ...)
 {
+	s_struct	*new;
+
+	new = ft_insert_params(format);
+
 	return (0);
 }
--- Ft_printf/ft_printf.h
+++ Ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/12 16:32:41 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/12 16:34:24 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/15 15:42:07 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,6 +15,15 @@
 # define FT_PRINTF_H
 
 # include <libft/libft.h>
+
+/*
+** Prototypes des structures
+*/
+typedef struc		s_struct
+{
+	int		argc;
+	char	**argv;
+}					s_struct;
 
 /*
 ** Prototypes des fonctions
