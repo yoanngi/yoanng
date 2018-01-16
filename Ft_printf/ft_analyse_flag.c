dfcb00824f59b368cb378819364eebd05dae9571
Yoann Ginet <yoginet@z4r3p1.le-101.fr>
2018-01-16 13:43:30 +0100

    backup

--- Ft_printf/ft_analyse_flag.c
+++ Ft_printf/ft_analyse_flag.c
@@ -6,39 +6,43 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/15 12:00:57 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/16 13:31:42 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/16 13:43:16 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int		where_is_it(char *str, int i)
+static void		ft_precision(s_struct *data, char c, int i)
 {
-	while (str[i] != '%')
-		i++;
-	return (i);
+	if (ft_isdigit(c) == 1)
+		data->prebefor = i;
+	else if (c == '.')
+		data->point = i;
+}
+
+static void		ft_flag(s_struct *data, char c, int i)
+{
+	if (c == '+')
+		data->flag_plus = i;
+	else if (c == '-')
+		data->flag_moins = i;
+	else if (c == '#')
+		data->flag_diese = i;
+	else if (c == ' ')
+		data->flag_space = i;
 }
 
 int				ft_analyse_flag(s_struct *data, int i)
 {
 	char	*str;
-	size_t	len;
 
 	str = ft_strdup(data->s);
-	len = ft_strlen(str);
-	i = 0;
-	while ((size_t)i <= len)
+	while (str[i])
 	{
-		i = where_is_it(str, i);
-		if (str[i + 1] == '+')
-			data->flag_plus = i;
-		else if (str[i + 1] == '-')
-			data->flag_moins = i;
-		else if (str[i + 1] == '#')
-			data->flag_diese = i;
-		else if (str[i + 1] == ' ')
-			data->flag_space = i;
+		if (str[i] == '%')
+			ft_precision(data, str[i + 1], i);
+			ft_flag(data, str[i + 1], i);
 		i++;
 	}
 	return (0);
