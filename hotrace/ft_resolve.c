41c641d72c9bcfec354e2b4a15defabe6f065926
yoanng <yoann.ginet@gmail.com>
2018-02-23 08:24:59 +0100

    back

--- hotrace/ft_resolve.c
+++ hotrace/ft_resolve.c
@@ -16,14 +16,15 @@
 
 static void		ft_insert_data_in_tab(void ***tab, t_lst *cpy)
 {
-	if (tab[cpy->hash][0] == NULL)
+	if (tab[cpy->hash][0] == (long)0)
 	{
 		tab[cpy->hash][0] = (long)cpy->hash;
 		tab[cpy->hash][1] = &cpy;
 		printf("Valeur tab[0][0] = %ld\n", (long)tab[0][0]);
 		printf("Valeur tab[0][1]= %p\n", tab[0][1]);
 	}
-//	else
+	else
+		printf("tab[cpy->hash][0] est deja pris !\n");
 //		creer liste chainer
 }
 
@@ -37,7 +38,7 @@ static void		**ft_malloc_tab(void **tab, int i, int nb)
 		tab[j] = (void *)malloc(sizeof(void) * nb + 1);
 		if (!tab[j])
 			return (NULL);
-		tab[j] = NULL;
+		tab[j] = (long)0;
 		j++;
 	}
 	return (tab);
@@ -48,6 +49,7 @@ int				ft_resolve(t_lst *start, int i)
 	t_lst	*cpy;
 	void	**tab_hash;
 
+	int j = 0;
 	cpy = start;
 	tab_hash = (void **)malloc(sizeof(void *) * i);
 	if (!tab_hash)
@@ -55,6 +57,9 @@ int				ft_resolve(t_lst *start, int i)
 	tab_hash = ft_malloc_tab(tab_hash, i, 2);
 	while (cpy)
 	{
+		printf("cpy->cle = %s, cpy->valeur = %s\n", cpy->cle, cpy->valeur);
+		cpy->hash = j;
+		j++;
 		ft_insert_data_in_tab(&tab_hash, cpy);
 		cpy = cpy->next;
 	}
