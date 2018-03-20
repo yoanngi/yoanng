e7c0a3b7ff68bd349bc595674a27c75583d80c03
Yoann Ginet <yoginet@z4r3p2.le-101.fr>
2018-03-20 16:37:05 +0100

    backup

--- ft_ls/srcs/ft_class.c
+++ ft_ls/srcs/ft_class.c
@@ -6,7 +6,7 @@
 /*   By: yoginet <yoginet@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/03/01 13:12:05 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/20 16:17:32 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/20 16:36:50 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -74,6 +74,33 @@ t_lst			*lst_sort_time(t_lst *lst)
 	}
 	return (lst);
 }
+
+/*
+t_lst			*lst_sort_time(t_lst *lst)
+{
+		return (NULL);
+	if (lst->next && (lst->date < lst->next->date))
+		lst = lst_swap(lst, lst->next);
+	else if (lst->next && lst->date == lst->next->date)
+		if (lst->next && (lst->date < lst->next->date))
+			lst = lst_swap(lst, lst->next);
+	lst->next = lst_sort_time(lst->next);
+	if (lst->next && (lst->date < lst->next->date))
+	{
+		lst = lst_swap(lst, lst->next);
+		lst->next = lst_sort_time(lst->next);
+	}
+	else if (lst->next && lst->date == lst->next->date)
+	{
+		if (lst->next && (lst->date < lst->next->date))
+		{
+			lst = lst_swap(lst, lst->next);
+			lst->next = lst_sort_time(lst->next);
+		}
+	}
+	return (lst);
+}
+*/
 
 /*
 **	How sort list
