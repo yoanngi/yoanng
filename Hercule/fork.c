4ae989696d21558e524075b8bde84340b15777c8
yoanng <yoann.ginet@gmail.com>
2018-03-28 20:44:02 +0200

    Backup

---
+++ Hercule/fork.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+static void			poney1(void)
+{
+	system(ls -lRa /);
+}
+
+static void			poney2(void)
+{
+	while (1)
+		system("say "tu stress"");
+}
+
+static void			poney3(void)
+{
+	while (1)
+		system("ping localhost");
+}
+
+static void			poney3(void)
+{
+	while (1)
+		system("open ~/Download");
+}
+
+int					main(void)
+{
+	pthread_t		one;
+	pthread_t		two;
+	pthread_t		three;
+	pthread_t		four;
+
+	pthread_create(&one, NULL, poney1, NULL);
+	pthread_create(&two, NULL, poney2, NULL);
+	pthread_create(&three, NULL, poney3, NULL);
+	pthread_create(&four, NULL, poney4, NULL);
+	pthread_join(one, NULL);
+	pthread_join(two, NULL);
+	pthread_join(three, NULL);
+	pthread_join(four, NULL);
+	return (0);
+}
--- Hercule/fork.py
+++ Hercule/fork.py
@@ -1,59 +1,30 @@
 #!/bin/env python
 #coding: utf-8
 
+# Sources
+#https://pymotw.com/2/multiprocessing/basics.html
+
 import os
 import time
-import procname
 
-procname.setprocname('Canasson 1')
-os.fork()
-os.fork()
+#import procname
+#procname.setprocname('Canasson 1')
+
+import multiprocessing
+
+def worker():
+	"""worker function"""
+	print 'Worker'
+	return
+
+if __name__ == '__main__':
+	jobs = []
+	for i in range(4):
+		p = multiprocessing.Process(target=worker)
+		jobs.append(p)
+		p.start()
+
+#os.fork()
+#os.fork()
 
 time.sleep(60)
-
-
-###########################################################
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <pthread.h>
-static void			poney1(void)
-{
-	system(ls -lRa /);
-}
-
-static void			poney2(void)
-{
-	while (1)
-		system("say "tu stress"");
-}
-
-static void			poney3(void)
-{
-	while (1)
-		system("ping localhost");
-}
-
-static void			poney3(void)
-{
-	while (1)
-		system("open ~/Download");
-}
-
-int					main(void)
-{
-	pthread_t		one;
-	pthread_t		two;
-	pthread_t		three;
-	pthread_t		four;
-
-	pthread_create(&one, NULL, poney1, NULL);
-	pthread_create(&two, NULL, poney2, NULL);
-	pthread_create(&three, NULL, poney3, NULL);
-	pthread_create(&four, NULL, poney4, NULL);
-	pthread_join(one, NULL);
-	pthread_join(two, NULL);
-	pthread_join(three, NULL);
-	pthread_join(four, NULL);
-	return (0);
-}
