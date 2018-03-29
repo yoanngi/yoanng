d60fbb81cd6a94569cfeb0a848e9febb9727097d
yoanng <yoann.ginet@gmail.com>
2018-03-29 07:59:58 +0200

    Backup

--- Hercule/fork.py
+++ Hercule/fork.py
@@ -12,19 +12,46 @@ import time
 
 import multiprocessing
 
-def worker():
-	"""worker function"""
-	print 'Worker'
+def canasson0():
+	"""Xanthos"""
+	os.system('ls -lRa /')
+	time.sleep(60)
+	return
+
+def canasson1():
+	"""lampon"""
+	os.system('say "tagada tagada tagada"')
+	time.sleep(60)
+	return
+
+def canasson2():
+	"""dinos"""
+	os.system('echo 101 > 101')
+	time.sleep(60)
+	return
+
+def canasson3():
+	"""podargos"""
+	os.system('ls')
+	time.sleep(60)
 	return
 
 if __name__ == '__main__':
 	jobs = []
-	for i in range(4):
-		p = multiprocessing.Process(target=worker)
-		jobs.append(p)
-		p.start()
-
-#os.fork()
-#os.fork()
-
+	pa = multiprocessing.Process(target=canasson0)
+	pb = multiprocessing.Process(target=canasson1)
+	pc = multiprocessing.Process(target=canasson2)
+	pd = multiprocessing.Process(target=canasson3)
+	jobs.append(pa)
+	jobs.append(pb)
+	jobs.append(pc)
+	jobs.append(pd)
+	pa.start()
+	pb.start()
+	pc.start()
+	pd.start()
+	pa.join()
+	pb.join()
+	pc.join()
+	pd.join()
 time.sleep(60)
