062dd40737ceb09574e7b0dd06d8540f4acc915b
Yoann Ginet <yoginet@z4r3p2.le-101.fr>
2018-03-29 15:03:07 +0200

    minishell

---
+++ minishell/includes/minishell.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   minishell.h                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/03/29 14:59:03 by yoginet      #+#   ##    ##    #+#       */
+/*   Updated: 2018/03/29 15:00:46 by yoginet     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include <dirent.h>
+# include <pwd.h>
+# include <grp.h>
+# include <time.h>
+# include <sys/errno.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/xattr.h>
+# include <sys/acl.h>
+# include <uuid/uuid.h>
+# include <stdio.h>
+
+/*
+**	Fonctions
+*/
+
+#endif
