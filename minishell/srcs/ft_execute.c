8c97d0d4d6e3cd18afff2062d64c7bb1a98dc43f
Yoann Ginet <yoginet@z4r4p1.le-101.fr>
2018-04-10 15:53:17 +0200

    Backup

--- minishell/srcs/ft_execute.c
+++ minishell/srcs/ft_execute.c
@@ -6,7 +6,7 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/10 14:30:14 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/10 15:21:35 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/10 15:53:07 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,7 +16,7 @@
 /*
 **	Fonction pour print la variable env
 */
-
+/****************************************************/
 static void		print_env(char **env)
 {
 	int i;
@@ -28,6 +28,7 @@ static void		print_env(char **env)
 		i++;
 	}
 }
+/****************************************************/
 
 /*
 **	Recupere la commande passer en parametre et la variable env
@@ -36,5 +37,5 @@ static void		print_env(char **env)
 void		ft_execute(char **line, char **env)
 {
 	print_env(env);
-	execve(*line, NULL, NULL);
+	execve(*line, &line[0], env);
 }
