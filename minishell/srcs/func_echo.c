d8fc93b4be2bf37937abd429945c300c55695f5c
yoanng <yoann.ginet@gmail.com>
2018-04-15 18:44:42 +0200

    Backup

--- minishell/srcs/func_echo.c
+++ minishell/srcs/func_echo.c
@@ -13,26 +13,42 @@
 
 #include "minishell.h"
 
-int		func_echo(char **line, t_struct *data)
+/*
+**	Split a modifier, probleme des retours a la lignes
+*/
+
+static char		**func_split_echo(char *line)
+{
+	char	**tab;
+	char	*tmp;
+
+	tmp = ft_strsub(line, 4, ft_strlen(line) - 4);
+	tab = ft_strsplit(tmp, '"');
+	ft_strdel(&tmp);
+	return (tab);
+}
+
+int				func_echo(char **line, t_struct *data)
 {
 	int		i;
 	char	**tab;
+	char	*echo;
 
 	i = 0;
-	tab = ft_strsplit(*line, ' ');
-	ft_strdel(&tab[0]);
+	tab = func_split_echo(*line);
 	while (data->tab_path[i])
 	{
-		tab[0] = ft_add_line(data->tab_path[i], "echo");
-		if ((ft_process(tab[0], tab)) > -1)
+		echo = ft_add_line(data->tab_path[i], "echo");
+		if ((ft_process(echo, tab)) > -1)
 		{
-			ft_strdel(&tab[0]);
+			ft_strdel(&echo);
 			ft_del_tab(tab);
 			return (0);
 		}
-		ft_strdel(&tab[0]);
+		ft_strdel(&echo);
 		i++;
 	}
+	ft_del_tab(tab);
 	ft_error(1, line);
 	return (1);
 }
