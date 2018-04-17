0370701f13ed064baf4fbfc018845ae44a7d9120
Yoann Ginet <yoginet@z4r3p2.le-101.fr>
2018-04-17 15:06:09 +0200

    Backup

--- minishell/srcs/func_echo.c
+++ minishell/srcs/func_echo.c
@@ -6,7 +6,7 @@
 /*   By: yoginet <yoginet@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/15 13:22:07 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/16 13:54:35 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/17 15:05:44 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,19 +14,57 @@
 #include "minishell.h"
 
 /*
-**	Split a modifier, probleme des retours a la lignes
+**	Part 2 : Split line for commande echo	
+*/
+/*
+static char		**func_split_echo_suite(char **tab)
+{
+	char	**new_tab;
+	int		len;
+	int		i;
+
+	new_tab = NULL;
+	len = ft_len_tab(tab);
+	i = 1;
+	while (i != len)
+	{
+		new_tab = ft_strsplit(tab[i], ' ');
+			
+	}
+	
+	
+	return (new_tab);
+}
+*/
+/*
+**	Part 1 : Split line for commande echo	
+**	34 = "
+**	92 = \
 */
 
 static char		**func_split_echo(char *line)
 {
 	char	**tab;
-	char	*tmp;
+	int		i;
+	int		quit;
 
-	tmp = ft_strsub(line, 4, ft_strlen(line) - 4);
-	tab = ft_strsplit(tmp, '"');
-	ft_strdel(&tmp);
+	i = 0;
+	quit = 0;
+	while (quit == 0 && ((size_t)i < ft_strlen(line)))
+	{
+		if (line[i] == 34 && line[i - 1] != 92)
+			quit = 1;
+		i++;
+	}
+	tab = ft_strsplit(line, ' ');
+//	if (quit == 1)
+//		tab = func_split_echo_suite(char **tab);
 	return (tab);
 }
+
+/*
+**	Core echo
+*/
 
 int				func_echo(char **line, t_struct *data)
 {
