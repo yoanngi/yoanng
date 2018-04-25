4bf233c3a2f6b203acdcca8f73c06d662c1eef8f
yoanng <yoann.ginet@gmail.com>
2018-04-25 07:13:14 +0200

    Backup

--- Hercule/proxy/proxy.c
+++ Hercule/proxy/proxy.c
@@ -6,30 +6,27 @@
 ** https://forum.hardware.fr/hfr/Programmation/C/socket-programmer-proxy-sujet_88089_1.htm
 */
 
-static void						proxy_suite(int fd)
+static void						proxy_suite(int fd, struct stockaddr_in *cli_addr)
 {
-	pid_t	pid;
+	int					client_fd;
 	char	buffer[512];
 	char	t1[512];
 	char	t2[512];
 	char	t3[10];
 
-	pid = fork();
+	client_fd = sizeof(cli_addr);
 	bzero((char*)buffer,512);
 	bzero((char*)t1,512);
 	bzero((char*)t2,512);
 	bzero((char*)t3,10);
-	if (pid == 0)
+	while (1)
 	{
-		while (1)
-		{
-			recv(fd,buffer,512,0);
-			sscanf(buffer,"%s %s %s",t1,t2,t3);
-
-		}
+		recv(fd,buffer,512,0);
+		sscanf(buffer,"%s %s %s",t1,t2,t3);
 	}
-	else
-		printf("Error: Process not run\n");
+//	if ((client_fd = accept(server_socket, (struct sockaddr *)NULL, NULL)) < 0)
+//		return (3);
+	close(client_fd);
 }
 
 static struct sockaddr_in		ft_server_socket(int p)
@@ -46,20 +43,17 @@ static struct sockaddr_in		ft_server_socket(int p)
 int								proxy(int p)
 {
 	int					server_socket;
-	int					client_fd;
 	struct sockaddr_in	s_addr;
 	struct sockaddr_in	c_addr;
 
 	s_addr = ft_server_socket(p);
+	bzero((char*)&c_addr, sizeof(c_addr));
 	if ((server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
 		return (1);
 	if (bind(server_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) < 0)
 		return (2);
 	listen(server_socket, 42);
-	if ((client_fd = accept(server_socket, (struct sockaddr *)NULL, NULL)) < 0)
-		return (3);
-	proxy_suite(client_fd);
-	close(client_fd);
+	proxy_suite(server_socket, c_addr)
 	close(server_socket);
 	return (0);
 }
