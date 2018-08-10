f986db1ebc73bb95a7e059815a4909cc68df4306
Yoann Ginet <yoginet@z4r7p1.le-101.fr>
2018-08-10 16:37:38 +0200

    Backup

--- 21sh/srcs/parsing/check_line_vide.c
+++ 21sh/srcs/parsing/check_line_vide.c
@@ -6,7 +6,7 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/26 12:06:01 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/08/10 10:38:34 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/08/10 16:34:16 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,9 +20,17 @@ int			ft_check_line_vide(char *str)
 	i = 0;
 	if (ft_strlen(str) == 0)
 		return (0);
+	if (ft_strstr(str, "\"") == NULL && ft_strstr(str, "\'") == NULL)
+	{
+		if (ft_strstr(str, ";;") != NULL)
+		{
+			ft_putstr_fd("21sh: syntax error near unexpected token `;;'\n", 2);
+			return (0);
+		}
+	}
 	while (str[i])
 	{
-		if (str[i] != ' ' && str[i] != ';')
+		if (str[i] != ' ' || str[i] != ';')
 			return (1);
 		i++;
 	}
--- 21sh/srcs/parsing/ft_error_parsing.c
+++ 21sh/srcs/parsing/ft_error_parsing.c
@@ -6,7 +6,7 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/17 10:26:53 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/28 10:50:50 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/08/10 16:36:56 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -39,7 +39,7 @@ static void		print_msg_error(char *str, int i)
 
 static int		ft_check_start_line(char *str)
 {
-	if (str[0] == '>' || str[0] == '<')
+	if ((str[0] == '>' || str[0] == '<') && (str[1] != '>' || str[1] != '<'))
 	{
 		print_msg_error(str, 1);
 		return (1);
@@ -65,33 +65,58 @@ static int		check_end_line(char *line)
 	return (0);
 }
 
-static int		check_regex_invalid(char *str, int i, int j, int ret)
+static int		check_str(char *str, int i)
 {
-	char	*invalid;
+	int		len;
 
-	invalid = ft_strdup("|;\"<>[]{}'!");
-	ret = 0;
+	len = ft_strlen(str);
+	if (i + 1 >= len)
+		return (0);
+	if (str[i] == '>' && str[i + 1] == '<')
+	{
+		print_msg_error("<", 2);
+		return (1);
+	}
+	if (str[i] == ';' && str[i + 1] == ';')
+	{
+		print_msg_error(";;", 2);
+		return (1);
+	}
+	if (str[i] == '!')
+	{
+		print_msg_error(NULL, 1);
+		return (1);
+	}
+	return (0);
+}
+
+static int		check_regex_invalid(char *str, int i)
+{
+	int		quote;
+	int		dquote;
+
+	quote = 0;
+	dquote = 0;
+	if (str == NULL)
+		return (1);
 	while (str[i])
 	{
-		j = 0;
-		while (invalid[j])
+		if (str[i] == '\'' && quote == 0)
+			quote = 1;
+		else if (str[i] == '\'' && quote == 1)
+			quote = 0;
+		else if  (str[i] == '\"' && dquote == 0)
+			dquote = 1;
+		else if (str[i] == '\"' && dquote == 1)
+			dquote = 0;
+		if (quote == 0 && dquote == 0)
 		{
-			if (invalid[j] == str[i])
-				ret = 1;
-			j++;
-		}
-		if (ret == 1)
-			ret = 0;
-		else
-		{
-			ft_strdel(&invalid);
-			return (0);
+			if (check_str(str, i) == 1)
+				return (1);
 		}
 		i++;
 	}
-	print_msg_error(str, 3);
-	ft_strdel(&invalid);
-	return (1);
+	return (0);
 }
 
 int				ft_nefaitrien(char **line)
@@ -106,7 +131,7 @@ int				ft_nefaitrien(char **line)
 	}
 	else if (check_end_line(*line) == 1)
 		return (1);
-	else if (check_regex_invalid(*line, 0, 0, 0) == 1)
+	else if (check_regex_invalid(*line, 0) == 1)
 		return (1);
 	return (0);
 }
